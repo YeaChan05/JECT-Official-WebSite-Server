26ede0d917133f8ead218201f58f312982cf0a5c
YeaChan05 <qkenrdl05@gmail.com>
2025-01-28 11:47:11 +0900

    feat: 이메일 템플릿 상수 설정
    템플릿파일을 상수값으로 조회 가능하도록 열거형 구현
    
    #5

---
+++ src/main/java/org/ject/support/external/email/EmailTemplate.java
@@ -0,0 +1,42 @@
+package org.ject.support.external.email;
+
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.ject.support.common.exception.GlobalErrorCode;
+import org.ject.support.common.exception.GlobalException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+@Getter
+@AllArgsConstructor
+public enum EmailTemplate {
+    ACCEPTANCE("email-template","ject notification"),
+    REJECTION("email-template","ject notification"),
+    CERTIFICATE("email-template","ject certificate");
+
+    private final String templateName;// template file name
+    private final String subject;// email subject
+
+    @PostConstruct
+    private void validateFileName() throws IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        List<String> templateFileNames = Arrays.stream(values()).map(EmailTemplate::getTemplateName).toList();
+
+        Set<String> actualFileNames = Arrays.stream(resolver.getResources("classpath:/templates/*.html"))
+                .map(Resource::getFilename)
+                .collect(Collectors.toSet());
+
+        for (String templateFileName : templateFileNames) {
+            if (!actualFileNames.contains(templateFileName + ".html")) {
+                throw new GlobalException(GlobalErrorCode.TEMPLATE_NOT_FOUND);
+            }
+        }
+    }
+}
