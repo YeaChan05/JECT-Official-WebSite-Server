54b50544debf6439653b7e7ce48074b398656432
YeaChan05 <qkenrdl05@gmail.com>
2025-01-28 17:00:26 +0900

    style: 코드 정렬
    #5

--- src/main/java/org/ject/support/external/email/MailSendException.java
+++ src/main/java/org/ject/support/external/email/MailSendException.java
@@ -4,7 +4,7 @@ import org.ject.support.common.exception.BusinessException;
 import org.ject.support.common.exception.ErrorCode;
 
 public class MailSendException extends BusinessException {
-  public MailSendException(final ErrorCode errorCode) {
-    super(errorCode);
-  }
+    public MailSendException(final ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
