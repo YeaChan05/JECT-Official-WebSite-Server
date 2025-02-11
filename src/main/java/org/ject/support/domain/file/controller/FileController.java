ac7c046730b5d306803069fd17338491822e574a
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-02-11 16:48:14 +0900

    feat: fileName에 @RequestBody 어노테이션 추가

--- src/main/java/org/ject/support/domain/file/controller/FileController.java
+++ src/main/java/org/ject/support/domain/file/controller/FileController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.ject.support.domain.file.dto.CreatePresignedUrlResponse;
 import org.ject.support.external.s3.S3Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class FileController {
 
     // TODO 인증 어노테이션 추가
     @PostMapping("/url")
-    public CreatePresignedUrlResponse createPresignedUrl(Long memberId, final String fileName) {
+    public CreatePresignedUrlResponse createPresignedUrl(Long memberId, @RequestBody final String fileName) {
         return s3Service.createPresignedUrl(memberId, fileName);
     }
 }
