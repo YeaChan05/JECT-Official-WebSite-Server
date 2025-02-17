8b9b787f64ba70be25f372597e6dba1e421ce614
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-02-17 21:59:08 +0900

    feat: 이메일 인증 기능 구현

---
+++ src/main/java/org/ject/support/domain/auth/AuthController.java
@@ -0,0 +1,25 @@
+package org.ject.support.domain.auth;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.ject.support.domain.auth.AuthDto.AuthCodeResponse;
+import org.ject.support.domain.auth.AuthDto.VerifyAuthCodeRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/verify-auth-code")
+    public AuthCodeResponse verifyAuthCode(HttpServletResponse response,
+                                           @RequestBody VerifyAuthCodeRequest request) {
+        return authService.verifyEmailByAuthCode(response, request.getName(), request.getEmail(),
+                request.getPhoneNumber(), request.getAuthCode());
+    }
+}
---
+++ src/main/java/org/ject/support/domain/auth/AuthService.java
@@ -0,0 +1,76 @@
+package org.ject.support.domain.auth;
+
+
+import static org.ject.support.domain.auth.AuthErrorCode.INVALID_AUTH_CODE;
+import static org.ject.support.domain.auth.AuthErrorCode.NOT_FOUND_AUTH_CODE;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.ject.support.common.security.jwt.JwtCookieProvider;
+import org.ject.support.common.security.jwt.JwtTokenProvider;
+import org.ject.support.domain.auth.AuthDto.AuthCodeResponse;
+import org.ject.support.domain.member.Member;
+import org.ject.support.domain.member.MemberDto.TempMemberJoinRequest;
+import org.ject.support.domain.member.MemberRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtCookieProvider jwtCookieProvider;
+
+    public AuthCodeResponse verifyEmailByAuthCode(HttpServletResponse response, String name, String email,
+                                                  String phoneNumber, String userInputCode) {
+        verifyAuthCode(email, userInputCode);
+
+        Member member = memberRepository.findByEmail(email).orElse(null);
+        if (member == null) {
+            member = createTempMember(name, email, phoneNumber);
+        }
+
+        Authentication authentication = jwtTokenProvider.createAuthenticationByMember(member);
+        String accessToken = jwtTokenProvider.createAccessToken(authentication, member.getId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(authentication);
+
+        addCookie(response, accessToken, refreshToken);
+
+        // TODO: 개발 시에만 json으로 토큰 반환. 추후 HTTPS 적용 및 실제 운영시에는 쿠키로만 전달 예정 (보안 이슈)
+        return new AuthCodeResponse(accessToken, refreshToken);
+    }
+
+    private Member createTempMember(String name,  String email, String phoneNumber) {
+        Member member = TempMemberJoinRequest.toEntity(name, email, phoneNumber);
+        return memberRepository.save(member);
+    }
+
+
+    // 쿠키 추가 메서드
+    private void addCookie(HttpServletResponse response, String accessToken, String refreshToken) {
+        response.addCookie(jwtCookieProvider.createAccessCookie(accessToken));
+        response.addCookie(jwtCookieProvider.createRefreshCookie(refreshToken));
+    }
+
+    public void verifyAuthCode(String key, String userInputCode) {
+        // redis에서 키 값으로 인증 번호 조회
+        String redisCode = redisTemplate.opsForValue().get(key);
+
+        // Redis에서 코드가 없는 경우
+        if (redisCode == null) {
+            throw new AuthException(NOT_FOUND_AUTH_CODE);
+        }
+
+        // 코드 불일치
+        if (!userInputCode.equals(redisCode)) {
+            throw new AuthException(INVALID_AUTH_CODE);
+        }
+
+        // 인증 성공
+        redisTemplate.delete(key);
+    }
+}
