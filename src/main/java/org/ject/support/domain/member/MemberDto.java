d1e973f6c914387ce7aab28da1a94cecacc0d611
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-02-17 20:50:46 +0900

    feat: 회원 엔티티 생성 시 요구하는 필드 추가

--- src/main/java/org/ject/support/domain/member/MemberDto.java
+++ src/main/java/org/ject/support/domain/member/MemberDto.java
@@ -1,7 +1,9 @@
 package org.ject.support.domain.member;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,22 +16,26 @@ public class MemberDto {
     @AllArgsConstructor
     public static class TempMemberJoinRequest {
 
+        @NotBlank
+        @Pattern(regexp = "^[가-힣]{1,5}$", message = "한글 1~5글자만 입력 가능합니다.")
+        private String name;
+
+        @NotBlank
+        @Pattern(regexp = "^010\\d{8}$", message = "010으로 시작하는 11자리 숫자를 입력하세요.")
+        private String phoneNumber;
+
         @NotNull
         @Size(max = 30)
         @Email(regexp = "^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]{1,10}\\.[a-zA-Z]{2,}$")
         private String email;
 
-        public static Member toEntity(String email) {
+        public static Member toEntity(String name, String email, String phoneNumber) {
             return Member.builder()
-                    .name("UNKNOWN")
-                    .phoneNumber(generateRandomPhoneNumber())
+                    .name(name)
+                    .phoneNumber(phoneNumber)
                     .email(email)
                     .role(Role.TEMP)
                     .build();
-        }
-
-        private static String generateRandomPhoneNumber() {
-            return "000" + (int) (Math.random() * 100000000);
         }
     }
 }
