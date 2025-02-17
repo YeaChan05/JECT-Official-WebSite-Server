5fbcf845ef0c4fd057ea58f6e6d506c3deaf7c4a
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-02-17 21:57:52 +0900

    feat: 이메일 전송 기능 구현

---
+++ src/main/java/org/ject/support/external/email/EmailController.java
@@ -0,0 +1,23 @@
+package org.ject.support.external.email;
+
+import lombok.RequiredArgsConstructor;
+import org.ject.support.common.security.AuthPrincipal;
+import org.ject.support.external.email.EmailDto.SendEmailRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailSendService emailSendService;
+
+    @PostMapping("/send-auth-email")
+    public void sendAuthEmail(@RequestBody SendEmailRequest sendEmailRequest) {
+        emailSendService.sendAuthCodeEmail(sendEmailRequest.getEmail());
+    }
+}
--- src/main/java/org/ject/support/external/email/EmailSendService.java
+++ src/main/java/org/ject/support/external/email/EmailSendService.java
@@ -1,20 +1,30 @@
 package org.ject.support.external.email;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.ject.support.common.util.Json2MapSerializer;
-import org.springframework.mail.MailException;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailSendService {
     private final JavaMailSender mailSender;
     private final MessageGenerator messageGenerator;
     private final Json2MapSerializer json2MapSerializer;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final Random random = new Random();
+
+    // 인증 번호 만료 시간 (초)
+    private static final long EXPIRE_TIME = 305;
+    public static final int AUTH_CODE_LENGTH = 6;
 
     /**
      * 단건 email 전송
@@ -37,9 +47,32 @@ public class EmailSendService {
     private void sendMail(final MimeMessagePreparator ...preparators) {
         try {
             mailSender.send(preparators);
-        } catch (MailException e) {
-            throw new MailSendException(MailErrorCode.MAIL_SEND_FAILURE);
+        } catch (org.springframework.mail.MailException e) {
+            throw new MailException(MailErrorCode.MAIL_SEND_FAILURE);
         }
+    }
+
+    public void sendAuthCodeEmail(String email) {
+        // 랜덤 6자리 숫자
+        String authCode = createAuthCode();
+
+        // Redis에 저장 (key: "email:이메일", value: 인증번호)
+        redisTemplate.opsForValue().set(email, authCode, Duration.ofSeconds(EXPIRE_TIME));
+        String storedCode = redisTemplate.opsForValue().get(email);
+        log.info("storedCode: " + storedCode);
+
+        sendEmail(email, EmailTemplate.CERTIFICATE, Map.of("to", email, "value", authCode));
+        // TODO: 정책 추가 시 인증 번호 전송 횟수 제한 로직 추가
+    }
+
+    private String createAuthCode() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < AUTH_CODE_LENGTH; i++) {
+            sb.append(random.nextInt(10));
+        }
+
+        return sb.toString();
     }
 
     public record MailSendRequest(
