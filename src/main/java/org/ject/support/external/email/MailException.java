978aa9c9196310c40d5c21865435734984c94473
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-02-17 21:43:16 +0900

    feat: 이메일 예외 처리 클래스명 변경 및 에러 코드 추가

--- src/main/java/org/ject/support/external/email/MailException.java
+++ src/main/java/org/ject/support/external/email/MailException.java
@@ -3,8 +3,8 @@ package org.ject.support.external.email;
 import org.ject.support.common.exception.BusinessException;
 import org.ject.support.common.exception.ErrorCode;
 
-public class MailSendException extends BusinessException {
-    public MailSendException(final ErrorCode errorCode) {
+public class MailException extends BusinessException {
+    public MailException(final ErrorCode errorCode) {
         super(errorCode);
     }
 }
