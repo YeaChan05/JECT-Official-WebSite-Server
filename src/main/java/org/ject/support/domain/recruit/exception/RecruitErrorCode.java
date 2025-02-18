ca9406a37a6ad81eea267274c9424fc8602ae138
YeaChan05 <qkenrdl05@gmail.com>
2025-02-18 20:23:28 +0900

    fix: 에러 메세지 수정
    #48

--- src/main/java/org/ject/support/domain/recruit/exception/RecruitErrorCode.java
+++ src/main/java/org/ject/support/domain/recruit/exception/RecruitErrorCode.java
@@ -7,7 +7,7 @@ import org.ject.support.common.exception.ErrorCode;
 @Getter
 @AllArgsConstructor
 public enum RecruitErrorCode implements ErrorCode {
-    NOT_FOUND("RECRUIT_NOT_FOUND", "채용 공고를 찾을 수 없습니다."),;
+    NOT_FOUND("RECRUIT_NOT_FOUND", "모집 공고를 찾을 수 없습니다."),;
     private final String code;
     private final String message;
 }
