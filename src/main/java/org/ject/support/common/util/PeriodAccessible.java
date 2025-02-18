266a3fe35ce2b7771a39463ab78b6caaa0b124e6
YeaChan05 <qkenrdl05@gmail.com>
2025-02-18 14:05:12 +0900

    feat: 접근 기한 제한 어노테이션 구현
    접근 기한을 제한해줄 어노테이션 구현
    
    #48

---
+++ src/main/java/org/ject/support/common/util/PeriodAccessible.java
@@ -0,0 +1,12 @@
+package org.ject.support.common.util;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(ElementType.METHOD)
+public @interface PeriodAccessible {
+}
