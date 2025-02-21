9201223019c2eda46cf7e2ed245eea10bb238338
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-02-21 14:15:51 +0900

    feat: 프로젝트 목록 조회 API의 page size 기본값을 20에서 30으로 변경

--- src/main/java/org/ject/support/domain/project/controller/ProjectController.java
+++ src/main/java/org/ject/support/domain/project/controller/ProjectController.java
@@ -5,6 +5,7 @@ import org.ject.support.domain.project.dto.ProjectResponse;
 import org.ject.support.domain.project.service.ProjectService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +19,8 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping
-    public Page<ProjectResponse> findProjects(@RequestParam String semester, Pageable pageable) {
+    public Page<ProjectResponse> findProjects(@RequestParam String semester,
+                                              @PageableDefault(size = 30) Pageable pageable) {
         return projectService.findProjectsBySemester(semester, pageable);
     }
 }
