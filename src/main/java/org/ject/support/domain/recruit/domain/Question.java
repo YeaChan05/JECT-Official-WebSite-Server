f99948939d064945356f0885764b6b8648be473b
YeaChan05 <qkenrdl05@gmail.com>
2025-02-23 18:00:23 +0900

    feat: enhance Question entity with Lombok annotations for better code readability

--- src/main/java/org/ject/support/domain/recruit/domain/Question.java
+++ src/main/java/org/ject/support/domain/recruit/domain/Question.java
@@ -2,11 +2,18 @@ package org.ject.support.domain.recruit.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.ject.support.domain.base.BaseTimeEntity;
 
+@Getter
+@Builder
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Question extends BaseTimeEntity {
 
     @Id
@@ -18,6 +25,7 @@ public class Question extends BaseTimeEntity {
 
     private String body;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recruit_id", nullable = false)
     private Recruit recruit;
