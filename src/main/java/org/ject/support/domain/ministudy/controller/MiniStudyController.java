69cac760f61d8e24c7e82816832ed121f1c80a3c
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-02-26 12:30:52 +0900

    feat: 미니스터디 조회 기능 구현

---
+++ src/main/java/org/ject/support/domain/ministudy/controller/MiniStudyController.java
@@ -0,0 +1,24 @@
+package org.ject.support.domain.ministudy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.ject.support.domain.ministudy.dto.MiniStudyResponse;
+import org.ject.support.domain.ministudy.service.MiniStudyService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ministudies")
+@RequiredArgsConstructor
+public class MiniStudyController {
+
+    private final MiniStudyService miniStudyService;
+
+    @GetMapping
+    public Page<MiniStudyResponse> findMiniStudies(@PageableDefault(size = 30) Pageable pageable) {
+        return miniStudyService.findMiniStudies(pageable);
+    }
+}
---
+++ src/main/java/org/ject/support/domain/ministudy/service/MiniStudyService.java
@@ -0,0 +1,21 @@
+package org.ject.support.domain.ministudy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ject.support.domain.ministudy.dto.MiniStudyResponse;
+import org.ject.support.domain.ministudy.repository.MiniStudyRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MiniStudyService {
+
+    private final MiniStudyRepository miniStudyRepository;
+
+    @Transactional
+    public Page<MiniStudyResponse> findMiniStudies(Pageable pageable) {
+        return miniStudyRepository.findMiniStudies(pageable);
+    }
+}
