c78b7ea4625dd55e6e2d8639e03708c1b0ccecb8
YeaChan05 <qkenrdl05@gmail.com>
2025-03-03 15:42:56 +0900

    feat: add inquireApplication method to ApplyService for retrieving recent temporary applications

--- src/main/java/org/ject/support/domain/recruit/service/ApplyUsecase.java
+++ src/main/java/org/ject/support/domain/recruit/service/ApplyUsecase.java
@@ -4,5 +4,7 @@ import java.util.Map;
 import org.ject.support.domain.member.JobFamily;
 
 public interface ApplyUsecase {
+    Map<String, String> inquireApplication(JobFamily jobFamily, Long memberId);
+
     void applyTemporary(JobFamily jobFamily, Long memberId, Map<String, String> answers);
 }
