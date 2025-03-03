5b08ab3ca8f3a4d177ff9922aea046a1d3fe8eed
YeaChan05 <qkenrdl05@gmail.com>
2025-03-03 15:44:31 +0900

    fix: change findLatestByMemberId method to return a list of TemporaryApplication

--- src/main/java/org/ject/support/domain/tempapply/repository/TemporaryApplicationRepository.java
+++ src/main/java/org/ject/support/domain/tempapply/repository/TemporaryApplicationRepository.java
@@ -1,10 +1,7 @@
 package org.ject.support.domain.tempapply.repository;
 
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
-import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import org.ject.support.domain.tempapply.domain.TemporaryApplication;
 import org.ject.support.external.dynamodb.domain.CompositeKey;
 import org.ject.support.external.dynamodb.repository.AbstractDynamoDbRepository;
@@ -22,10 +19,8 @@ public class TemporaryApplicationRepository extends AbstractDynamoDbRepository<T
     }
 
     //TODO 2025 02 21 10:08:04 : 최신 문서임을 꼭 application level에서 확인해야 하는가?
-    public Optional<TemporaryApplication> findLatestByMemberId(String memberId) {
+    public List<TemporaryApplication> findLatestByMemberId(String memberId) {
         CompositeKey partitionKey = new CompositeKey(TemporaryApplication.PK_PREFIX, memberId);
-        List<TemporaryApplication> applications =
-                findByPartitionWithSortType(partitionKey, TemporaryApplication.SK_PREFIX);
-        return applications.stream().max(Comparator.comparing(app -> LocalDateTime.parse(app.getSk().getPostfix())));
+        return findByPartitionWithSortType(partitionKey, TemporaryApplication.SK_PREFIX);
     }
 }
