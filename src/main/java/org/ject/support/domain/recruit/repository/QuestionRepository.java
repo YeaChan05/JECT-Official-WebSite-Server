f70e11a8b5f7316ee0160f273d6d386335604ea9
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-03-15 01:06:19 +0900

    feat: 지원서 문항 조회 기능 구현

---
+++ src/main/java/org/ject/support/domain/recruit/repository/QuestionRepository.java
@@ -0,0 +1,7 @@
+package org.ject.support.domain.recruit.repository;
+
+import org.ject.support.domain.recruit.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionQueryRepository {
+}
