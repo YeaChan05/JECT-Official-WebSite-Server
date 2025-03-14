bcc3f4ec404fa0b08f2aa164190cf59251edc8ac
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-03-15 01:05:39 +0900

    refactor: 불필요한 클래스 제거

--- src/test/java/org/ject/support/domain/recruit/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ject.support.domain.recruit.repository;
-
-import org.ject.support.domain.recruit.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
