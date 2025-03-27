452d9499e55f1b25f535f49e6fd2ce0cf120c9c2
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-03-27 16:22:22 +0900

    test: Recruit의 isInvalidQuestionId 단위테스트

--- src/test/java/org/ject/support/domain/recruit/domain/RecruitTest.java
+++ src/test/java/org/ject/support/domain/recruit/domain/RecruitTest.java
@@ -27,4 +27,33 @@ class RecruitTest {
         // then
         assertThat(isRecruitingPeriod).isTrue();
     }
+
+    @Test
+    @DisplayName("is invalid question id")
+    void is_invalid_question_id() {
+        // given
+        Recruit recruit = Recruit.builder()
+                .semester("2025-1")
+                .jobFamily(JobFamily.BE)
+                .startDate(LocalDateTime.now())
+                .endDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        Question question = Question.builder()
+                .id(1L)
+                .sequence(1)
+                .inputType(Question.InputType.TEXT)
+                .isRequired(true)
+                .title("title")
+                .recruit(recruit)
+                .build();
+
+        recruit.addQuestion(question);
+
+        // when
+        boolean isInvalidQuestionId = recruit.isInvalidQuestionId(2L);
+
+        // then
+        assertThat(isInvalidQuestionId).isTrue();
+    }
 }
