fe3217eb8a515027899e0800cbb6e8200de6d8ac
YeaChan05 <qkenrdl05@gmail.com>
2025-03-29 15:10:43 +0900

    feat: implement SemesterRegistrationService for semester registration functionality

---
+++ src/main/java/org/ject/support/domain/recruit/service/SemesterRegistrationService.java
@@ -0,0 +1,30 @@
+package org.ject.support.domain.recruit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ject.support.domain.recruit.domain.Semester;
+import org.ject.support.domain.recruit.dto.RegisterRecruitEvent;
+import org.ject.support.domain.recruit.dto.SemesterRegisterRequest;
+import org.ject.support.domain.recruit.repository.SemesterRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SemesterRegistrationService implements SemesterRegisterUsecase {
+    private final SemesterRepository semesterRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+
+    @Override
+    @Transactional
+    public void registerSemester(final SemesterRegisterRequest request) {
+        // semester 등록
+        Semester semester = Semester.builder()
+                .name(request.name())
+                .build();
+        semesterRepository.save(semester);
+        // semester 기준으로 recruit 등록
+        eventPublisher.publishEvent(new RegisterRecruitEvent(semester.getId(),request.recruitRegisterRequests()));
+    }
+}
