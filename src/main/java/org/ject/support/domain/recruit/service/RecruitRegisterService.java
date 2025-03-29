5e4e88990a91f065bda56eb8c4d5620a097da67f
YeaChan05 <qkenrdl05@gmail.com>
2025-03-29 15:10:51 +0900

    feat: add RecruitRegisterService and RegisterRecruitEvent for handling recruitment registration

---
+++ src/main/java/org/ject/support/domain/recruit/service/RecruitRegisterService.java
@@ -0,0 +1,25 @@
+package org.ject.support.domain.recruit.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.ject.support.domain.recruit.domain.Recruit;
+import org.ject.support.domain.recruit.dto.RegisterRecruitEvent;
+import org.ject.support.domain.recruit.repository.RecruitRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Service
+@RequiredArgsConstructor
+public class RecruitRegisterService {
+    private final RecruitRepository recruitRepository;
+
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)// recruit 저장 실패시 전체 트랜잭션 rollback
+    public void handleRegisterRecruitEvent(RegisterRecruitEvent event) {
+        Long semesterId = event.id();
+        List<Recruit> recruits = event.recruitRegisterRequests().stream()
+                .map(request -> request.toEntity(semesterId))
+                .toList();
+        recruitRepository.saveAll(recruits);
+    }
+}
