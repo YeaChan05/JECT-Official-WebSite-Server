47892727c7ea02704e328931f3836bd5298aeca4
YeaChan05 <qkenrdl05@gmail.com>
2025-04-01 11:06:37 +0900

    fix: update id generation strategy for Semester entity to use GenerationType.IDENTITY

--- src/main/java/org/ject/support/domain/recruit/domain/Semester.java
+++ src/main/java/org/ject/support/domain/recruit/domain/Semester.java
@@ -3,6 +3,7 @@ package org.ject.support.domain.recruit.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -20,8 +21,7 @@ import org.ject.support.domain.base.BaseTimeEntity;
 @Builder
 public class Semester extends BaseTimeEntity {
     @Id
-    @GeneratedValue
-    @Column(name = "semester_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "semester_name", length = 20, nullable = false, unique = true)
