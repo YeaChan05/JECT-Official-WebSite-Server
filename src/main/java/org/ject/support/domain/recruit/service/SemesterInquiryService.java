16f1f72ec58e736e9679ed7c076bf8206cf65594
YeaChan05 <qkenrdl05@gmail.com>
2025-04-03 21:47:54 +0900

    refactor: simplify getAllSemesters method by using toList() for brevity

--- src/main/java/org/ject/support/domain/recruit/service/SemesterInquiryService.java
+++ src/main/java/org/ject/support/domain/recruit/service/SemesterInquiryService.java
@@ -1,7 +1,6 @@
 package org.ject.support.domain.recruit.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.ject.support.domain.recruit.dto.SemesterListResponse;
 import org.ject.support.domain.recruit.repository.SemesterRepository;
@@ -21,6 +20,6 @@ public class SemesterInquiryService implements SemesterInquiryUsecase {
     public List<SemesterListResponse> getAllSemesters() {
         return semesterRepository.findAll().stream()
                 .map(SemesterListResponse::from)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
