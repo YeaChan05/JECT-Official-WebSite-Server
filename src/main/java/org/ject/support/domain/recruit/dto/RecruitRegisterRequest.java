1800b1f393021df630ad88fe637ad29324c73e38
YeaChan05 <qkenrdl05@gmail.com>
2025-04-03 21:46:33 +0900

    refactor: remove date fields from RecruitRegisterRequest for simplification

--- src/main/java/org/ject/support/domain/recruit/dto/RecruitRegisterRequest.java
+++ src/main/java/org/ject/support/domain/recruit/dto/RecruitRegisterRequest.java
@@ -3,12 +3,9 @@ package org.ject.support.domain.recruit.dto;
 import java.time.LocalDateTime;
 import org.ject.support.domain.member.JobFamily;
 import org.ject.support.domain.recruit.domain.Recruit;
-import org.springframework.format.annotation.DateTimeFormat;
 
 public record RecruitRegisterRequest(JobFamily jobFamily,
-                                     @DateTimeFormat(pattern = "yyyy-MM-dd HH")
                                      LocalDateTime startDate,
-                                     @DateTimeFormat(pattern = "yyyy-MM-dd HH")
                                      LocalDateTime endDate) {
     public Recruit toEntity(Long ongoingSemesterId) {
         return Recruit.builder()
