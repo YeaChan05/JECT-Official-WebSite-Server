b93bdbdae37cdafe517994f012d5ae336975d619
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-04-05 04:00:24 +0900

    refactor: StringListConverter에서 ObjectMapper 직접 생성하도록 변경

--- src/main/java/org/ject/support/common/util/StringListConverter.java
+++ src/main/java/org/ject/support/common/util/StringListConverter.java
@@ -4,16 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
-import lombok.RequiredArgsConstructor;
 import org.ject.support.common.exception.GlobalErrorCode;
 import org.ject.support.common.exception.GlobalException;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+
 public class StringListConverter implements AttributeConverter<List<String>, String> {
 
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
