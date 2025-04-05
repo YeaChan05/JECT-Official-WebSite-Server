f4f4ef578190723cd0423721e51aee7a52326278
YeaChan05 <qkenrdl05@gmail.com>
2025-04-05 21:35:27 +0900

    test: refactor SemesterControllerTest for improved readability and consistency

--- src/test/java/org/ject/support/domain/recruit/controller/SemesterControllerTest.java
+++ src/test/java/org/ject/support/domain/recruit/controller/SemesterControllerTest.java
@@ -19,14 +19,11 @@ import org.ject.support.domain.recruit.repository.RecruitRepository;
 import org.ject.support.domain.recruit.repository.SemesterRepository;
 import org.ject.support.testconfig.AuthenticatedUser;
 import org.ject.support.testconfig.IntegrationTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -47,102 +44,87 @@ class SemesterControllerTest {
     @Autowired
     private SemesterRepository semesterRepository;
 
-    @Nested
-    @DisplayName("기수 등록 API 테스트")
-    class SemesterRegisterTests {
-        @Test
-        @AuthenticatedUser(isAdmin = true)
-        @Rollback
-        @DisplayName("학기와 모집 포지션을 등록한다")
-        void register_semester() throws Exception {
-            // given
-            SemesterRegisterRequest request = new SemesterRegisterRequest("2023-2학기",
-                    List.of(
-                            new RecruitRegisterRequest(JobFamily.BE,
-                                    LocalDateTime.of(2025, 4, 1, 0, 0, 0),
-                                    LocalDateTime.of(2025, 4, 2, 0, 0, 0)),
-                            new RecruitRegisterRequest(JobFamily.FE,
-                                    LocalDateTime.of(2025, 4, 1, 0, 0, 0),
-                                    LocalDateTime.of(2025, 4, 2, 0, 0, 0)
-                            )));
+    @Test
+    @AuthenticatedUser(isAdmin = true)
+    @DisplayName("학기와 모집 포지션을 등록한다")
+    void register_semester() throws Exception {
+        // given
+        SemesterRegisterRequest request = new SemesterRegisterRequest("2023-2학기",
+                List.of(
+                        new RecruitRegisterRequest(JobFamily.BE,
+                                LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+                                LocalDateTime.of(2025, 4, 2, 0, 0, 0)),
+                        new RecruitRegisterRequest(JobFamily.FE,
+                                LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+                                LocalDateTime.of(2025, 4, 2, 0, 0, 0)
+                        )));
 
-            String reqJson = objectMapper.writeValueAsString(request);
-            // when
-            mockMvc.perform(post("/semesters")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(reqJson))
-                    .andExpect(status().isOk())
-                    .andExpect(
-                            content().string(containsString("SUCCESS"))
-                    );
-            TestTransaction.flagForCommit();
-            TestTransaction.end();
+        String reqJson = objectMapper.writeValueAsString(request);
+        // when
+        mockMvc.perform(post("/semesters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(reqJson))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().string(containsString("SUCCESS"))
+                );
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
 
-            // then
-            assertThat(semesterRepository.findAll()).hasSize(1);
-            assertThat(recruitRepository.findAll()).hasSize(2);
-        }
-
-        @Test
-        @AuthenticatedUser(isAdmin = true)
-        @Rollback
-        @DisplayName("중복된 JobFamily로 학기를 등록할 수 없다")
-        void register_semester_with_duplicate_jobFamily() throws Exception {
-            // given
-            SemesterRegisterRequest request = new SemesterRegisterRequest("2023-2학기",
-                    List.of(
-                            new RecruitRegisterRequest(JobFamily.BE,
-                                    LocalDateTime.of(2025, 4, 1, 0, 0, 0),
-                                    LocalDateTime.of(2025, 4, 2, 0, 0, 0)),
-                            new RecruitRegisterRequest(JobFamily.BE,
-                                    LocalDateTime.of(2025, 4, 1, 0, 0, 0),
-                                    LocalDateTime.of(2025, 4, 2, 0, 0, 0))// 중복된 jobFamily
-                    ));
-
-            String reqJson = objectMapper.writeValueAsString(request);
-            // when
-            mockMvc.perform(post("/semesters")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(reqJson))
-                    .andExpect(status().isOk())
-                    .andDo(print())
-                    .andExpect(
-                            content().string(containsString("DUPLICATED_JOB_FAMILY"))
-                    );
-
-            // then
-            assertThat(semesterRepository.findAll()).hasSize(0);
-            assertThat(recruitRepository.findAll()).hasSize(0);
-        }
+        // then
+        assertThat(semesterRepository.findAll()).hasSize(1);
+        assertThat(recruitRepository.findAll()).hasSize(2);
     }
 
-    @Nested
-    @DisplayName("학기 조회 API 테스트")
-    class SemesterInquiryTests {
-        
-        @BeforeEach
-        void setUp() {
-            // 테스트 데이터 준비
-            semesterRepository.save(Semester.builder()
-                    .name("2023-1학기")
-                    .build());
-            
-            semesterRepository.save(Semester.builder()
-                    .name("2023-2학기")
-                    .build());
-        }
+    @Test
+    @AuthenticatedUser(isAdmin = true)
+    @DisplayName("중복된 JobFamily로 학기를 등록할 수 없다")
+    void register_semester_with_duplicate_jobFamily() throws Exception {
+        // given
+        SemesterRegisterRequest request = new SemesterRegisterRequest("2023-2학기",
+                List.of(
+                        new RecruitRegisterRequest(JobFamily.BE,
+                                LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+                                LocalDateTime.of(2025, 4, 2, 0, 0, 0)),
+                        new RecruitRegisterRequest(JobFamily.BE,
+                                LocalDateTime.of(2025, 4, 1, 0, 0, 0),
+                                LocalDateTime.of(2025, 4, 2, 0, 0, 0))// 중복된 jobFamily
+                ));
 
-        @Test
-        @DisplayName("모든 기수 목록을 조회한다")
-        void getAllSemesters() throws Exception {
-            // when & then
-            mockMvc.perform(get("/semesters"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpectAll(
-                            content().string(containsString("2023-1학기")),
-                            content().string(containsString("2023-2학기"))
-                    );
-        }
+        String reqJson = objectMapper.writeValueAsString(request);
+        // when
+        mockMvc.perform(post("/semesters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(reqJson))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(
+                        content().string(containsString("DUPLICATED_JOB_FAMILY"))
+                );
+
+        // then
+        assertThat(semesterRepository.findAll()).hasSize(0);
+        assertThat(recruitRepository.findAll()).hasSize(0);
+    }
+
+    @Test
+    @DisplayName("모든 기수 목록을 조회한다")
+    void getAllSemesters() throws Exception {
+        //given
+        Semester semester1 = Semester.builder()
+                .name("2025-1학기")
+                .build();
+        Semester semester2 = Semester.builder()
+                .name("2025-2학기")
+                .build();
+        semesterRepository.saveAll(List.of(semester1, semester2));
+        // when & then
+        mockMvc.perform(get("/semesters"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpectAll(
+                        content().string(containsString("2025-1학기")),
+                        content().string(containsString("2025-2학기"))
+                );
     }
 }
