a9b5e200f202f4af6f97f4e8f41900b99c9fd792
king_0417 <73704053+gywns0417@users.noreply.github.com>
2025-04-13 00:39:39 +0900

    feat: refactor cookie creation methods for access and refresh tokens (#169)

--- src/main/java/org/ject/support/common/security/jwt/JwtCookieProvider.java
+++ src/main/java/org/ject/support/common/security/jwt/JwtCookieProvider.java
@@ -17,21 +17,21 @@ public class JwtCookieProvider {
 
     public Cookie createRefreshCookie(String refreshToken) {
         String cookieName = "refreshToken";
-        Cookie cookie = new Cookie(cookieName, refreshToken);
-        cookie.setHttpOnly(true);
-        //cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge((int)(refreshExpirationTime / 1000));
-        return cookie;
+        return createCookie(cookieName, refreshToken);
     }
 
     public Cookie createAccessCookie(String accessToken) {
         String cookieName = "accessToken";
-        Cookie cookie = new Cookie(cookieName, accessToken);
+        return createCookie(cookieName, accessToken);
+    }
+
+    private Cookie createCookie(String cookieName, String token) {
+        Cookie cookie = new Cookie(cookieName, token);
         cookie.setHttpOnly(true);
-        //cookie.setSecure(true); TODO : HTTPS 적용 시 적용 가능
+        cookie.setSecure(true);
         cookie.setPath("/");
-        cookie.setMaxAge((int)(accessExpirationTime / 1000));
+        cookie.setMaxAge((int)(refreshExpirationTime / 1000));
+
         return cookie;
     }
 }
